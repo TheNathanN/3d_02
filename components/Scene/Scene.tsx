c45692514d5341eb4ff3affccea2a10bdc7194d7
nathan <thenathannicholson@gmail.com>
2023-01-15 13:48:46 -0800

    Remove OrbitControls and setup leva to control the camera

--- components/Scene/Scene.tsx
+++ components/Scene/Scene.tsx
@@ -1,24 +1,63 @@
 import React from "react"
-import Box from "../Box"
 import { useFrame } from "@react-three/fiber"
-import { useControls } from "leva"
 import { useGLTF, OrbitControls } from "@react-three/drei"
+import Box from "../Box"
+import { useControls } from "leva"
 
 useGLTF.preload("/assets/scene.glb")
 
 const Scene = () => {
   const scene = useGLTF("/assets/scene.glb")
+  const { rotationX, rotationY, rotationZ, positionX, positionY, positionZ } =
+    useControls("Camera", {
+      rotationX: {
+        value: -0.5,
+        min: -Math.PI,
+        max: Math.PI,
+        step: 0.1,
+      },
+      rotationY: {
+        value: 0.8,
+        min: -Math.PI,
+        max: Math.PI,
+        step: 0.01,
+      },
+      rotationZ: {
+        value: 0.38,
+        min: -Math.PI,
+        max: Math.PI,
+        step: 0.01,
+      },
+      positionX: {
+        value: 5.559419082866227,
+        min: -10,
+        max: 10,
+        step: 0.01,
+      },
+      positionY: {
+        value: 2.9919200717879257,
+        min: -10,
+        max: 10,
+        step: 0.1,
+      },
+      positionZ: {
+        value: 4.831241119392819,
+        min: -10,
+        max: 10,
+        step: 0.1,
+      },
+    })
 
-  // useFrame(({ camera }) => {
-  //   console.log(camera)
-  // })
+  useFrame(({ camera }) => {
+    camera.position.set(positionX, positionY, positionZ)
+    camera.rotation.set(rotationX, rotationY, rotationZ)
+  })
 
   return (
     <>
       <color attach="background" args={["black"]} />
-      <OrbitControls />
-      {/* <ambientLight />
-      <pointLight position={[10, 10, 10]} /> */}
+      {/* <ambientLight /> */}
+      {/* <pointLight position={[10, 10, 10]} /> */}
       {/* <Box position={[-1.2, 0, 0]} /> */}
       <primitive object={scene.scene} />
     </>
